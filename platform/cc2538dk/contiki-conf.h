d07bf57c62b5344abf2751afe37934e7bed33227
Lars Boegild Thomsen <lth@netcompartner.com>
2015-01-07 09:52:08 +0800

    Building and working

--- platform/cc2538dk/contiki-conf.h
+++ platform/cc2538dk/contiki-conf.h
@@ -386,11 +386,11 @@ typedef uint32_t rtimer_clock_t;
  */
 /* RF Config */
 #ifndef IEEE802154_CONF_PANID
-#define IEEE802154_CONF_PANID           0x5449 /**< Default PAN ID: TI */
+#define IEEE802154_CONF_PANID           0xABCD
 #endif
 
 #ifndef CC2538_RF_CONF_CHANNEL
-#define CC2538_RF_CONF_CHANNEL              25
+#define CC2538_RF_CONF_CHANNEL              26
 #endif /* CC2538_RF_CONF_CHANNEL */
 
 #ifndef CC2538_RF_CONF_AUTOACK
